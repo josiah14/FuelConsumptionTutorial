0f455c29f7f8883b3f51e5584439c1932f312e22
josiah14 <praenato14@gmail.com>
2013-01-20 02:58:17 -0500

    OnClick fail

--- ConsCalc/gen/idig/za/net/conscalc/R.java
+++ ConsCalc/gen/idig/za/net/conscalc/R.java
@@ -14,17 +14,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int buttonAddRecord=0x7f070002;
-        public static final int buttonCalculateConsumption=0x7f070003;
-        public static final int buttonDeleteRecord=0x7f070004;
-        public static final int buttonEditRecord=0x7f070005;
-        public static final int buttonSaveRegistration=0x7f070009;
-        public static final int editTextRegistration=0x7f070008;
-        public static final int mainLinearLayout=0x7f070006;
-        public static final int menu_settings=0x7f07000a;
-        public static final int relativeLayoutMainMenu=0x7f070001;
-        public static final int textViewGetRegistrationLabel=0x7f070007;
-        public static final int textViewMenuHeading=0x7f070000;
+        public static final int add_record=0x7f070000;
+        public static final int buttonAddRecord=0x7f070004;
+        public static final int buttonCalculateConsumption=0x7f070005;
+        public static final int buttonDeleteRecord=0x7f070006;
+        public static final int buttonEditRecord=0x7f070007;
+        public static final int buttonSaveRegistration=0x7f07000b;
+        public static final int calc_menu=0x7f070001;
+        public static final int editTextRegistration=0x7f07000a;
+        public static final int mainLinearLayout=0x7f070008;
+        public static final int menu_settings=0x7f07000c;
+        public static final int relativeLayoutMainMenu=0x7f070003;
+        public static final int textViewGetRegistrationLabel=0x7f070009;
+        public static final int textViewMenuHeading=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
