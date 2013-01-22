b817d9e461982ea897ef519d35842e1fd225fda4
josiah14 <praenato14@gmail.com>
2013-01-22 02:29:03 -0500

    add record layout started

--- ConsCalc/gen/idig/za/net/conscalc/R.java
+++ ConsCalc/gen/idig/za/net/conscalc/R.java
@@ -10,23 +10,45 @@ package idig.za.net.conscalc;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int Heading0=0x7f040001;
+        public static final int Heading1=0x7f040000;
+        public static final int Heading2=0x7f040002;
+        public static final int Heading3=0x7f040003;
+        public static final int fifteen=0x7f040004;
+        public static final int ten=0x7f040005;
+        public static final int twelve=0x7f040006;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int add_record=0x7f070000;
-        public static final int buttonAddRecord=0x7f070004;
-        public static final int buttonCalculateConsumption=0x7f070005;
-        public static final int buttonDeleteRecord=0x7f070006;
-        public static final int buttonEditRecord=0x7f070007;
-        public static final int buttonSaveRegistration=0x7f07000b;
-        public static final int calc_menu=0x7f070001;
-        public static final int editTextRegistration=0x7f07000a;
-        public static final int mainLinearLayout=0x7f070008;
-        public static final int menu_settings=0x7f07000c;
-        public static final int relativeLayoutMainMenu=0x7f070003;
-        public static final int textViewGetRegistrationLabel=0x7f070009;
-        public static final int textViewMenuHeading=0x7f070002;
+        public static final int TextViewAddRecordDetails=0x7f080002;
+        public static final int TextViewAddRecordHeading=0x7f080001;
+        public static final int TextViewDateLabel=0x7f080005;
+        public static final int TextViewRegNumber=0x7f080003;
+        public static final int add_record=0x7f080000;
+        public static final int buttonAddRecord=0x7f080011;
+        public static final int buttonCalculateConsumption=0x7f080012;
+        public static final int buttonDeleteRecord=0x7f080013;
+        public static final int buttonEditRecord=0x7f080014;
+        public static final int buttonPickDate=0x7f080006;
+        public static final int buttonSaveRecord=0x7f080004;
+        public static final int buttonSaveRegistration=0x7f080018;
+        public static final int calc_menu=0x7f08000e;
+        public static final int editTextCost=0x7f080009;
+        public static final int editTextLitres=0x7f080007;
+        public static final int editTextOdometer=0x7f080008;
+        public static final int editTextRegistration=0x7f080017;
+        public static final int mainLinearLayout=0x7f080015;
+        public static final int menu_settings=0x7f080019;
+        public static final int relativeLayoutMainMenu=0x7f080010;
+        public static final int textView5=0x7f08000d;
+        public static final int textViewCost=0x7f08000a;
+        public static final int textViewGetRegistrationLabel=0x7f080016;
+        public static final int textViewLitres=0x7f08000c;
+        public static final int textViewMenuHeading=0x7f08000f;
+        public static final int textViewOdometer=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -38,22 +60,34 @@ public final class R {
         public static final int get_registration=0x7f030006;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
-        public static final int add_record=0x7f060001;
-        public static final int calculate_consumption=0x7f060002;
-        public static final int delete_record=0x7f060003;
-        public static final int edit_record=0x7f060004;
+        public static final int activity_main=0x7f070000;
+        public static final int add_record=0x7f070001;
+        public static final int calculate_consumption=0x7f070002;
+        public static final int delete_record=0x7f070003;
+        public static final int edit_record=0x7f070004;
     }
     public static final class string {
-        public static final int add_record=0x7f040003;
-        public static final int app_name=0x7f040000;
-        public static final int calculate_consumption=0x7f040005;
-        public static final int delete_record=0x7f040004;
-        public static final int edit_record=0x7f040006;
-        public static final int get_registration=0x7f040008;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int splash_screen=0x7f040007;
+        public static final int add_record=0x7f050003;
+        public static final int add_record_details=0x7f050009;
+        public static final int app_name=0x7f050000;
+        public static final int calculate_consumption=0x7f050005;
+        public static final int cost_label=0x7f05000e;
+        public static final int cost_text=0x7f050012;
+        public static final int date_label=0x7f05000b;
+        public static final int date_text=0x7f05000f;
+        public static final int delete_record=0x7f050004;
+        public static final int edit_record=0x7f050006;
+        public static final int get_registration=0x7f050008;
+        public static final int hello_world=0x7f050001;
+        public static final int litres_label=0x7f05000d;
+        public static final int litres_text=0x7f050011;
+        public static final int menu_settings=0x7f050002;
+        public static final int odometer_label=0x7f05000c;
+        public static final int odometer_text=0x7f050010;
+        public static final int pick_date=0x7f050013;
+        public static final int registration_number=0x7f05000a;
+        public static final int save_record=0x7f050014;
+        public static final int splash_screen=0x7f050007;
     }
     public static final class style {
         /** 
@@ -77,10 +111,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
