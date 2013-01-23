58e06c5d076285c80d12c400f741a384259af85c
josiah14 <praenato14@gmail.com>
2013-01-23 02:31:28 -0600

    Dynamically updating date and reg number.

--- ConsCalc/src/idig/za/net/conscalc/AddRecord.java
+++ ConsCalc/src/idig/za/net/conscalc/AddRecord.java
@@ -1,24 +1,30 @@
 package idig.za.net.conscalc;
 
 import java.util.Calendar;
+import java.util.Date;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 
 public class AddRecord extends Activity {
 	/*************************************************************************************************************
-	 * Class Members 																																														 *
+	 * Class Members 																							 *
 	 *************************************************************************************************************/
 	// create variable called date of type long
 	private long date;
 	// create variable called stringDate of type String
 	private String stringDate;
+	// declare a file for storing the preferences
+	public static final String PREFS_NAME = "MyPrefsFile";
 	
 	/*************************************************************************************************************
-	 * Nested Classes																																														 *
+	 * Nested Classes																							 *
 	 *************************************************************************************************************/
 	// Debugging Variables
 	private static class Debugger {
@@ -36,13 +42,53 @@ public class AddRecord extends Activity {
 	}
 	
 	/*************************************************************************************************************
-	 * Methods          																																														 *
+	 * Methods          																					     *
 	 *************************************************************************************************************/
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add_record);
+		
+		// set textViews with values
+		TextView tvDate = (TextView) findViewById(R.id.TextViewDisplayDate);
+		tvDate.setText(getCurDateTime());
+		
+		TextView tvRegistrationNumber  = (TextView) findViewById(R.id.TextViewRegNumber);
+		tvRegistrationNumber.setText(getRegistrationNumber());
+	}
+	
+	private SharedPreferences getPreferences() {
+		return getSharedPreferences(PREFS_NAME, 0);
+	}
+	
+	private String getCurDateTime() {
 		final Calendar cal = Calendar.getInstance();
+		// get date as long to store in database
+		date = cal.getTimeInMillis();
+		
+		// for checking - convert long to string
+		Date curDate = new Date(date);
+		stringDate = curDate.toString();
+		Log.i(Debugger.TAG, "the long date is " + date);
+		Log.i(Debugger.TAG, "the stringDate = " + stringDate);
+		return stringDate;
+	}
+	
+	private String getRegistrationNumber() {
+		SharedPreferences settings = getPreferences();
+		if (!settings.getBoolean("registrationIsSet", false)) {
+			Log.e(Debugger.TAG, "the registration date is not set.");
+			return "";
+		} else {
+			String registrationNumber = settings.getString("registrationNumber", null);
+			if (registrationNumber == null) {
+				Log.e(Debugger.TAG, "the registration number was null.");
+				return "";
+			} else {
+				Log.i(Debugger.TAG, "the registration number retrieved was \"" + registrationNumber + "\"");
+				return registrationNumber;
+			}
+		}
 	}
 
 }
